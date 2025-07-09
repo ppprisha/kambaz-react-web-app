13d0d92be19d420ada8e3dea5b419f31b7ea45e6
ppprisha <prisha.sujin@gmail.com>
2025-07-09 02:35:27 -0400

    again

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,8 @@ export default function Labs() {
     return (
         <div id="wd-labs">
             <h1>Labs</h1>
+            <h2>Prisha Sujin Kumar</h2>
+
             <TOC />
             <Routes>
                 <Route path="/" element={<Navigate to="Lab1" />} />
