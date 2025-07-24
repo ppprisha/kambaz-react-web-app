92f4afc588dc0b611e301e6bb21b6795022f781e
ppprisha <prisha.sujin@gmail.com>
2025-07-24 04:04:49 -0400

    a3 javascript

--- src/Kambaz/Database/index.ts
+++ src/Kambaz/Database/index.ts
@@ -2,4 +2,5 @@ import courses from "./courses.json";
 import users from "./users.json";
 import enrollment from "./enrollments.json";
 import modules from "./modules.json"
-export { courses, users, enrollment, modules };
+import assignments from "./assignments.json"
+export { courses, users, enrollment, modules, assignments };
