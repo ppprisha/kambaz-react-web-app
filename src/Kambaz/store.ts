4d0ee4060452be31b555bd86021cb7f484389933
ppprisha <prisha.sujin@gmail.com>
2025-08-16 06:04:46 -0400

    pushing again

--- src/Kambaz/store.ts
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import accountReducer from "./Account/reducer";
-const store = configureStore({
-  reducer: {
-     account: accountReducer,
-  },
-});
-export default store;
