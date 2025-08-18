5a4db02a6b71ea3c9c42dbbf7c0c2f21e6336b33
ppprisha <prisha.sujin@gmail.com>
2025-08-17 20:43:08 -0400

    fixed

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -3,36 +3,25 @@ import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
 
+  if(links.length == 1) {
   return (
-    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      {!currentUser && (
-        <>
-          <Link
-            to="/Kambaz/Account/Signin"
-            id="wd-account-signin-link"
-            className="list-group-item active border border-0"
-          >
-            Signin
-          </Link>
-          <Link
-            to="/Kambaz/Account/Signup"
-            id="wd-account-signup-link"
-            className="list-group-item text-danger border border-0"
-          >
-            Signup
-          </Link>
-        </>
-      )}
-      {currentUser && (
-        <Link
-          to="/Kambaz/Account/Profile"
-          id="wd-account-profile-link"
-          className="list-group-item text-danger border border-0"
-        >
-          Profile
-        </Link>
-      )}
+    <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+    <Link to="/Kambaz/Account/Profile" id="wd-profile-link"
+      className="list-group-item text-danger border border-0">Profile</Link>
+  </div>
+);
+  }
+  else {
+    return (
+      <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+      <Link to="/Kambaz/Account/Signin" id="wd-signin-link"
+        className="list-group-item active border border-0">Sign in</Link>
+      <Link to="/Kambaz/Account/Signup" id="wd-signup-link"
+        className="list-group-item text-danger border border-0">Sign up</Link>
     </div>
   );
+    }
 }
+
