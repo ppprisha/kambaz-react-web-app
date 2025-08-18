8eb81828cf070797186c073204d9a5c0988c3a16
ppprisha <prisha.sujin@gmail.com>
2025-08-17 20:13:01 -0400

    fixing

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -1,46 +1,43 @@
 import axios from "axios";
-
-const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
-if (!REMOTE_SERVER) throw new Error("VITE_REMOTE_SERVER not set! Check Netlify env variables.");
-
-const axiosWithCredentials = axios.create({
-  baseURL: REMOTE_SERVER,
-  withCredentials: true,
-});
-
-export const USERS_API = "/api/users";
-
-export const signup = async (user: any) => {
-  const { data } = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
-  return data;
-};
-
-export const signin = async (credentials: any) => {
-  const { data } = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
-  return data;
-};
-
-export const signout = async () => {
-  const { data } = await axiosWithCredentials.post(`${USERS_API}/signout`);
-  return data;
-};
-
-export const profile = async () => {
-  const { data } = await axiosWithCredentials.get(`${USERS_API}/profile`);
-  return data;
-};
-
-export const updateUser = async (user: any) => {
-  const { data } = await axiosWithCredentials.put(`${USERS_API}/${user._id}`, user);
-  return data;
-};
-
+const axiosWithCredentials = axios.create({ withCredentials: true });
+export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+export const USERS_API = `${REMOTE_SERVER}/api/users`;
 export const findMyCourses = async () => {
-  const { data } = await axiosWithCredentials.get(`${USERS_API}/current/courses`);
+  const { data } = await axiosWithCredentials.get(
+    `${USERS_API}/current/courses`
+  );
   return data;
 };
-
+export const signin = async (credentials: any) => {
+  const response = await axiosWithCredentials.post(
+    `${USERS_API}/signin`,
+    credentials
+  );
+  return response.data;
+};
+export const profile = async () => {
+  const response = await axiosWithCredentials.post(`${USERS_API}/profile`);
+  return response.data;
+};
+export const signup = async (user: any) => {
+  const response = await axiosWithCredentials.post(`${USERS_API}/signup`, user);
+  return response.data;
+};
+export const signout = async () => {
+  const response = await axiosWithCredentials.post(`${USERS_API}/signout`);
+  return response.data;
+};
+export const updateUser = async (user: any) => {
+  const response = await axiosWithCredentials.put(
+    `${USERS_API}/${user._id}`,
+    user
+  );
+  return response.data;
+};
 export const createCourse = async (course: any) => {
-  const { data } = await axiosWithCredentials.post(`${USERS_API}/current/courses`, course);
+  const { data } = await axiosWithCredentials.post(
+    `${USERS_API}/current/courses`,
+    course
+  );
   return data;
 };
