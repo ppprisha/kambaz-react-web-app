2a4d47d65a42b8a6fa23d7d36f5795df424c40e1
ppprisha <prisha.sujin@gmail.com>
2025-08-17 20:38:41 -0400

    fixed

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -1,64 +1,36 @@
+import FormControl from "react-bootstrap/esm/FormControl";
+import { Link } from "react-router";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import {useNavigate } from "react-router-dom";
 import { setCurrentUser } from "./reducer";
-
-import { FormControl, Button } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { Button } from "react-bootstrap";
 import * as client from "./client";
+
 
 export default function Signin() {
   const [credentials, setCredentials] = useState<any>({});
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
   const signin = async () => {
-    try {
-      const user = await client.signin(credentials);
-      console.log("Signin response:", user);
-
-      if (!user || !user._id) {
-        alert("Invalid credentials or server didn’t return a user");
-        return;
-      }
-
-      dispatch(setCurrentUser(user));
-      navigate("/Kambaz/Dashboard");
-    } catch (err) {
-      console.error("Signin failed:", err);
-    }
+    const user =  await client.signin(credentials);
+    if (!user) return;
+    dispatch(setCurrentUser(user));
+    navigate("/Kambaz/Dashboard");
   };
-
-
-
   return (
-    <div id="wd-signin-screen">
+    <div id="wd-signin-screen" className="p-3">
       <h1>Sign in</h1>
-      <FormControl
-        value={credentials.username || ""}
-        onChange={(e) =>
-          setCredentials({ ...credentials, username: e.target.value })
-        }
-        className="mb-2"
-        placeholder="username"
-        id="wd-username"
-      />
-      <FormControl
-        value={credentials.password || ""}
-        onChange={(e) =>
-          setCredentials({ ...credentials, password: e.target.value })
-        }
-        className="mb-2"
-        placeholder="password"
-        type="password"
-        id="wd-password"
-      />
-
-      <Button onClick={signin} id="wd-signin-btn" className="w-100">
-        Sign in
-      </Button>
-      <Link id="wd-signup-link" to="/Kambaz/Account/Signup">
-        Sign up
-      </Link>
-    </div>
-  );
-}
+      <FormControl defaultValue={credentials.username}
+             onChange={(e) => setCredentials({ ...credentials, username: e.target.value })}
+             id="wd-username"
+             placeholder="username"
+             className="mb-2"/><br />
+      <FormControl defaultValue={credentials.password}
+             onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
+             id="wd-password"
+             placeholder="password" type="password"
+             className="mb-2"/><br />
+      <Button onClick={signin} id="wd-signin-btn" className="w-100" > Sign in </Button>
+      <Link id="wd-signup-link" to="/Kambaz/Account/Signup">Sign up</Link>
+    </div> );}
